3e9fb4391391bb4dfa97300e5175034332cf8270
aleixMerced <mgaleix@gmail.com>
2024-12-17 01:41:09 +0100

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,16 +5,15 @@ import { AuthGuard } from './auth.guard';
 import { PaginaIdemComponent } from './pagina-idem/pagina-idem.component'; // Asegúrate de crear este componente
 import { LoginModalComponent} from './login-modal/login-modal.component';
 
-export const routes: Routes = [
-  { path: '', component: AppComponent },  // Ruta principal
-  { path: 'pagina-idem', component: PaginaIdemComponent },  // Ruta para otra página
-  { path: 'login-modal', component: LoginModalComponent}
+
 import { PresentacioComponent } from './presentacio/presentacio.component';
 
 export const routes: Routes = [
   {path: 'presentacio', component: PresentacioComponent},
   { path: 'app', component: AppComponent, canActivate: [AuthGuard]},  // Ruta principal
-  { path: 'pagina-idem', component: PaginaIdemComponent }  // Ruta para otra página
+  { path: 'pagina-idem', component: PaginaIdemComponent },  // Ruta para otra página
+  { path: '', component: AppComponent },  // Ruta principal
+  { path: 'login-modal', component: LoginModalComponent}
 ];
 
 @NgModule({
